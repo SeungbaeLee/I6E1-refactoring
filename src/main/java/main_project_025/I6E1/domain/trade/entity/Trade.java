650e664b3423e08cba1ae8d55fb9cf56e806b4ab
SeungbaeLee <aha224610@gmail.com>
2024-03-06 09:20:53 +0900

    refactor : setter 제거, builder 적용, update 메소드 작성

--- src/main/java/main_project_025/I6E1/domain/trade/entity/Trade.java
+++ src/main/java/main_project_025/I6E1/domain/trade/entity/Trade.java
@@ -2,19 +2,19 @@ package main_project_025.I6E1.domain.trade.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import main_project_025.I6E1.domain.commission.entity.Commission;
-import main_project_025.I6E1.global.auditable.Auditable;
 import main_project_025.I6E1.domain.member.entity.Member;
+import main_project_025.I6E1.global.auditable.Auditable;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Setter
 @Entity
 @SQLDelete(sql = "UPDATE Trade SET deleted = true WHERE trade_id = ?")
 @Where(clause = "deleted = false")//where절에 반드시 포함되는 조건 설정 -> deleted = false -> 지워지지 않은 로우 -> 하지만 nativeQuery에는 적용 X
@@ -47,18 +47,17 @@ public class Trade extends Auditable {
     @Column(name = "status", nullable = false)
     private Status status = Status.Waiting_Acceptance;
 
-    public void setMember(Member member) {
-        this.member = member;
-        if (!member.getTrades().contains(this)) {
-            member.getTrades().add(this);
-        }
-    }
-
-    public void setCommission(Commission commission) {//C
+    @Builder
+    public Trade(Commission commission, Member member, String title, String content, String authorEmail, Status status) {
         this.commission = commission;
-        if (!commission.getTrades().contains(this)) {
-            commission.getTrades().add(this);
-        }
+        this.member = member;
+        this.title = title;
+        this.content = content;
+        this.authorEmail = authorEmail;
+        this.status = status;
     }
 
+    public void updateStatus(Status status) {
+        this.status = status;
+    }
 }
