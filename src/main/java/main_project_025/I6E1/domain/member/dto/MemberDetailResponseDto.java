107ae6ad9a5ccb48d32aadd2c64c8b0c184361bc
SeungbaeLee <aha224610@gmail.com>
2024-03-09 00:38:41 +0900

    refactor : fromEntity 작성

--- src/main/java/main_project_025/I6E1/domain/member/dto/MemberDetailResponseDto.java
+++ src/main/java/main_project_025/I6E1/domain/member/dto/MemberDetailResponseDto.java
@@ -1,6 +1,8 @@
 package main_project_025.I6E1.domain.member.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+import main_project_025.I6E1.domain.member.entity.Member;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,4 +15,25 @@ public class MemberDetailResponseDto {
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
     private List<String> roles;
+
+    @Builder
+    public MemberDetailResponseDto(Long memberId, String email, String nickname, LocalDateTime createdAt, LocalDateTime modifiedAt, List<String> roles) {
+        this.memberId = memberId;
+        this.email = email;
+        this.nickname = nickname;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.roles = roles;
+    }
+
+    public static MemberDetailResponseDto fromEntity(Member member) {
+        return MemberDetailResponseDto.builder()
+                .memberId(member.getMemberId())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .roles(member.getRoles())
+                .createdAt(member.getCreatedAt())
+                .modifiedAt(member.getModifiedAt())
+                .build();
+    }
 }
